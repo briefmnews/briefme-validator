34f9a793948d82d0549811c4d98ec68aa2dd6b1a
Thomas C <thomas.carbillet@gmail.com>
2019-06-13 10:18:58 +0200

    feat(release): release 0.1.1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ tests_require = [
 
 setup(
     name='briefme-validator',
-    version='0.1',
+    version='0.1.1',
     description='A simple validator.',
     long_description='''
 A simple validator, check mail for example.
