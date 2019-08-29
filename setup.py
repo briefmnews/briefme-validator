bfef4f513cc2c5bc75c190f66fcc66cf663743a0
Thomas C <thomas.carbillet@gmail.com>
2019-08-29 13:04:13 +0200

    feat(): update setup.py

--- setup.py
+++ setup.py
@@ -1,50 +1,28 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# This file is part of briefme-validator.
-# https://github.com/briefmnews/briefme-validator
-
-# Licensed under the MIT license:
-# http://www.opensource.org/licenses/MIT-license
-# Copyright (c) 2015, Hobbestigrou <hobbestigrou@erakis.eu>
-
 from setuptools import setup, find_packages
-
-tests_require = [
-    "mock",
-    "nose",
-    "coverage",
-    "yanc",
-    "preggy",
-    "tox",
-    "ipdb",
-    "coveralls",
-    "sphinx",
-]
 
 setup(
     name="briefme-validator",
     version="0.1.3",
-    description="A simple validator.",
+    description="An email validator.",
     long_description="""A simple validator, check mail for example.""",
-    keywords="dns, mail",
-    author="Hobbestigrou",
-    author_email="hobbestigrou@erakis.eu",
-    url="",
-    license="MIT",
+    keywords="dns, email, validator",
+    author="Brief.me",
+    author_email="tech@brief.me",
+    url="https://github.com/briefmnews/briefme-validator",
+    license=None,
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Operating System :: Unix",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: Implementation :: PyPy",
+        "Framework :: Django",
+        "Framework :: Django :: 2.2",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
         "Operating System :: OS Independent",
     ],
     packages=find_packages(),
     include_package_data=False,
-    install_requires=["dnspython"],
+    install_requires=["dnspython3", "Django>=2.0"],
     entry_points={"console_scripts": []},
+    zip_safe=False,
 )
